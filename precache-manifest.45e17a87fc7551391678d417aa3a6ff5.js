9f0b3e0a7a2a5e211aa948c959636dc7dd25da97
shahreazebd <shahreaz@evidentbd.com>
2022-07-07 14:30:41 +0600

    Updates

---
+++ precache-manifest.45e17a87fc7551391678d417aa3a6ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ba3b3f943ce4ecb8a647b1e27b6ffc",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "02d5de5b4412298b7b93",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d39e68e1.chunk.css"
+  },
+  {
+    "revision": "9a0f749434cbfd8c0e30",
+    "url": "/react-frontend-dev-portfolio/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "02d5de5b4412298b7b93",
+    "url": "/react-frontend-dev-portfolio/static/js/2.58e5d36d.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/react-frontend-dev-portfolio/static/js/2.58e5d36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0f749434cbfd8c0e30",
+    "url": "/react-frontend-dev-portfolio/static/js/main.b89ab532.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
